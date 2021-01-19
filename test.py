01d04d3c09c35d7649e31ac1cb20f72fee159e9f
xsk666 <47235404+xsk666@users.noreply.github.com>
2021-01-20 02:59:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # coding=gbk
 import requests
 import time
-req = requests.get('https://sc.ftqq.com/SCU79675Tbfd23351bd3ed5501aae715beddfbdbf5e3a123f8fb98.send?text=github action测试&desp=github服务器时间' +
+requests.get('https://sc.ftqq.com/SCU79675Tbfd23351bd3ed5501aae715beddfbdbf5e3a123f8fb98.send?text=github action测试&desp=github服务器时间' +
                    str(time.asctime(time.localtime(time.time()))))
 
