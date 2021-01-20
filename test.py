63f02fbc2dc91191a93a83aa512ef6fb50cb8b22
xsk666 <47235404+xsk666@users.noreply.github.com>
2021-01-20 12:39:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# coding=gbk
+# coding=utf-8
 import requests
 import time
 req = requests.get('https://sc.ftqq.com/SCU79675Tbfd23351bd3ed5501aae715beddfbdbf5e3a123f8fb98.send?text=测试时间&desp=github时间\n' +
