2cba462d3279ec86b08552d47d58df94596dc676
xsk666 <3104182180@qq.com>
2021-01-28 08:17:34 +0800

    mian

--- main/index.py
+++ main/index.py
@@ -13,6 +13,7 @@ print("开始 "+time.strftime("%Y/%m/%d")+" 的打卡任务\n")
 files = open(os.getcwd() + "/main/day.txt", 'r+')
 if (files.read() == time.strftime("%Y/%m/%d")):
     print("今日已打卡")
+    shutil.rmtree(os.getcwd()+"/main/__pycache__/")
     sys.exit()
 
 requests.get("https://sc.ftqq.com/SCU79675Tbfd23351bd3ed5501aae715beddfbdbf5e3a123f8fb98.send?text=开始 "+time.strftime("%Y/%m/%d")+" 自动打卡任务&desp=[点我查看运行状况](https://github.com/xsk666/autopost/actions)")
