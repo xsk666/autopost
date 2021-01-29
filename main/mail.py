b37addfe0b0f04853f492010c70fcb39ef53d96d
xsk666 <3104182180@qq.com>
2021-01-29 09:13:01 +0800

    mian

--- main/mail.py
+++ main/mail.py
@@ -6,13 +6,15 @@ import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.utils import formataddr
+
+
 def send(mail, username):
     # 发送到的邮箱地址，收件人的名称
     try:
         sender = '3104182180@qq.com'  # 发件人邮箱账号
         msg = MIMEText("To："+username+"\n\t自动健康打卡成功通知\n\t通知发送时间：" + time.strftime("%Y/%m/%d %H:%M")+"\n\n\t\tFrom：运行在github的开源项目", 'plain', 'utf-8')  # 填写邮件内容
         # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-        msg['From'] = formataddr(["xsk666", sender])
+        msg['From'] = formataddr(["自动打卡", sender])
         msg['To'] = formataddr([username, mail])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
         msg['Subject'] = "健康打卡成功通知"  # 邮件的主题，也可以说是标题
         server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
