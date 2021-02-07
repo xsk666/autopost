5e9037c6b824e6131077724ad23a4e33dd30142c
xsk666 <3104182180@qq.com>
2021-02-07 16:18:32 +0800

    main

--- main/helper.py
+++ main/helper.py
@@ -12,40 +12,42 @@ api = 'lightapp.weishao.com.cn'
 dat = "schoolcode=chzu&username=203135&password=Aa336699&verifyValue=&verifyKey=203135_chzu&ssokey="
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 url = 'https://lightapp.weishao.com.cn/check/reportstatistics'
+try:
+    url2 = requests.get(url, allow_redirects=False).headers['Location']
+    res = requests.get(url2, allow_redirects=False).headers
+    cook = res['set-cookie']
+    url3 = hapi + res['Location']
+    head = {
+        'Host': api,
+        'cache-control': 'max-age=0',
+        'Content-Length': '92',
+        'Upgrade-Insecure-Requests': '1',
+        'Origin': 'null',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': UA,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN, zh;q = 0.9',
+        "Cookie": cook
+    }
+    url4 = hapi + requests.post(url3, data=dat, headers=head, allow_redirects=False).headers['Location']
 
-url2 = requests.get(url, allow_redirects=False).headers['Location']
-res = requests.get(url2, allow_redirects=False).headers
-cook = res['set-cookie']
-url3 = hapi + res['Location']
-head = {
-    'Host': api,
-    'cache-control': 'max-age=0',
-    'Content-Length': '92',
-    'Upgrade-Insecure-Requests': '1',
-    'Origin': 'null',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': UA,
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN, zh;q = 0.9',
-    "Cookie": cook
-}
-url4 = hapi + requests.post(url3, data=dat, headers=head, allow_redirects=False).headers['Location']
-
-head2 = {
-    'Host': api,
-    'cache-control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'Origin': 'null',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': UA,
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN, zh;q = 0.9',
-    "Cookie": cook
-}
-url5 = requests.get(url4, headers=head2, allow_redirects=False).headers['Location']
-cook = "Hm_lvt_2897656ea377e58fb1af08554ed019b4=1611801447,1611805930,1611815687,1611818213;" + requests.get(url5, headers=head2, allow_redirects=False).headers['set-cookie'].split("; ")[0]
+    head2 = {
+        'Host': api,
+        'cache-control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'Origin': 'null',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': UA,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN, zh;q = 0.9',
+        "Cookie": cook
+    }
+    url5 = requests.get(url4, headers=head2, allow_redirects=False).headers['Location']
+    cook = "Hm_lvt_2897656ea377e58fb1af08554ed019b4=1611801447,1611805930,1611815687,1611818213;" + requests.get(url5, headers=head2, allow_redirects=False).headers['set-cookie'].split("; ")[0]
+except Exception:
+    print("程序出现错误")
 
 
 def off(lists, id):
@@ -88,7 +90,7 @@ num = []
 for i in range(0, 3):
     num.append(str(off(lists, classes[i])))
 print("共有"+str(len(lists))+"人未打卡\n网工201 共"+num[0]+"人\n网工202 共"+num[1]+"人\n智能20  共"+num[2]+"人\n")
-#print(lists)
+# print(lists)
 
 if (len(lists) == 0):
     print("所有人打卡完成")
--- main/index.py
+++ main/index.py
@@ -20,7 +20,7 @@ if (files.read() == time.strftime("%Y/%m/%d")):
 f2 = open(os.getcwd()+"/main/users.json", 'r', encoding='utf-8')
 info = json.loads(f2.read())
 f2.close()
-for i in range(0, 1):
+for i in range(0, len(info)):
     if(info[i].get("enable")=='true'):
         print("开始为 "+info[i].get("name") + " 打卡...")
         # 随机UA
@@ -30,25 +30,22 @@ for i in range(0, 1):
         f.close()
         # 获取用户cookie
         try:
-            #cook = sign.login(info[i], UA)
-            cook=info[i].get("cookie")
-            
+            cook = sign.login(info[i], UA)
             main.run(info[i], UA, cook)
         except Exception:
             print("为 " + info[i].get("name") + " 打卡失败")
 
 print("打卡结束")
-'''
+
 # 回到文件头部，清除重写
 files.seek(0)
 files.truncate()
 files.write(time.strftime("%Y/%m/%d"))
 files.close()
-'''
+
 shutil.rmtree(os.getcwd()+"/main/__pycache__/")
-'''
+
 try:
     requests.get("https://sc.ftqq.com/SCU79675Tbfd23351bd3ed5501aae715beddfbdbf5e3a123f8fb98.send?text=开始 "+time.strftime("%Y/%m/%d")+" 自动打卡任务&desp=[点我查看运行状况](https://github.com/xsk666/autopost/actions)")
 except:
     print("error")
-'''
