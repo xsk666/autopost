41d705822d7fcd52c150b4857002aae1dd9e2f7b
xsk666 <3104182180@qq.com>
2021-03-02 13:26:37 +0800

    main

--- test/paolu/qq点赞.py
+++ test/paolu/qq点赞.py
@@ -1,24 +1,16 @@
 # coding=utf-8
-import time,requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 # 模拟浏览器打开网站
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
-# window电脑本地
-# driver = webdriver.Chrome("./chromedriver.exe")
-driver.get("http://8dsw8.com/")
-driver.find_element_by_xpath("//option[contains(text(),'『福利』免费活动区')]").click()
-time.sleep(1)
-driver.find_elements_by_tag_name("option")[56].click()
-time.sleep(0.5)
+driver.get("http://8dsw8.com/?cid=1&tid=42")
+WebDriverWait(driver, 10, 0.5).until(EC.presence_of_element_located((By.ID, "inputvalue")))
 driver.find_element_by_id("inputvalue").send_keys("3104182180")
-time.sleep(0.5)
 driver.find_element_by_id("submit_buy").click()
-print("点赞成功")
-
-# requests.get("https://sc.ftqq.com/SCU79675Tbfd23351bd3ed5501aae715beddfbdbf5e3a123f8fb98.send?text=每日QQ点赞成功&desp=时间为" +time.strftime("%Y/%m/%d")).json()
-
