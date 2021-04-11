cf63c2aa71971dec48513221dacf1f1e8c3d290f
xsk666 <3104182180@qq.com>
2021-04-12 00:16:18 +0800

    main

--- main/helper.py
+++ main/helper.py
@@ -4,15 +4,15 @@ import time
 
 import requests
 
-import post
-import sign
-
 hapi = "https://api.weishao.com.cn"
 api = 'lightapp.weishao.com.cn'
 dat = "schoolcode=chzu&username=203135&password=203135&verifyValue=&verifyKey=203135_chzu&ssokey="
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 url = 'https://lightapp.weishao.com.cn/check/reportstatistics'
 oauth = "/oauth/authorize?client_id=pqZ3wGM07i8R9mR3&redirect_uri=https://lightapp.weishao.com.cn/check/reportstatistics&response_type=code&scope=base_api&state=ruijie"
+qmsg = "11a4ed5a314c66df757718ba36fea180"
+# 康康,网工202通知群
+group = ["1134347178"]  # ,"219045731"]
 
 
 def login():
@@ -67,37 +67,47 @@ head = {
     "Cookie": cook
 }
 list = requests.post(url1, json=data, headers=head).json().get("data").get("tree")
-txt = ""
-for i in range(0, len(list)):
+# 推送结果
+tui = "每日打卡督促完成啦\n\n|    班级    | 结果 |\n"
+for i in range(1):  # 0, len(list)):
     classes = list[i]
     data2 = {"type": "org", "identity": "", "para": {"organization_id": classes['unionid'], "organization_path_str": "3311,3312,3313,3314,3320"},
              "date": time.strftime("%Y-%m-%d"), "activityid": "5599", "flag": 0, "domain": "chzu", "stucode": "203135"}
     head['Content-Length'] = str(len(str(data2)))
     li = requests.post(url1, json=data2, headers=head).json().get("data")
-    print(classes.get("tree_name"), str(li.get("orgUserCount") - li.get("reportCount")) + "人未打卡")
+    txt = classes.get("tree_name") + " " + str(li.get("orgUserCount") - li.get("reportCount")) + "人未打卡\n"
+    print(txt)
     if li.get("orgUserCount") != li.get("reportCount"):
         text = "| "
+        x = 0
         for user in li.get("users"):
             if user.get("is_report") == 0:
-                text += user.get("user_name") + " | "
                 # 添加未打卡学生信息到列表
                 nodaka.append({"stucode": user.get("user_id"), "password": user.get("user_id"), 'schoolcode': 'chzu'})
-        print(text + "\n")
-        txt += "{}   {}人未打卡\n{}".format(classes.get("tree_name"), li.get("orgUserCount") - li.get("reportCount"), text)
-        if i != len(list) - 1:
-            txt += "\n\n"
-# qmsg酱推送到QQ
-# requests.get("https://qmsg.zendee.cn/send/11a4ed5a314c66df757718ba36fea180?msg=" + txt).json()
+                # 3人换一行，x用于计数
+                text += user.get("user_name") + " | "
+                x += 1
+                if x == 3:
+                    text += "\n| "
+                    x = 0
+        print(text)
+        txt += text
 
+        res = "失败"
+        if requests.get("https://qmsg.zendee.cn/group/" + qmsg + "?msg=" + txt + "&qq=" + group[0]).json().get("success"):
+            res = "成功"
+        tui += "| {} | {} |\n".format(classes.get("tree_name"), res)
+
+requests.get("https://qmsg.zendee.cn/send/" + qmsg + "?msg=" + tui).json()
 if len(nodaka) == 0:
     print("所有人打卡完成")
     sys.exit()
-
-for i in range(0, len(nodaka)):
-    print("开始为 " + nodaka[i].get("stucode") + " 打卡")
-    try:
-        cook = sign.login(nodaka[i], UA)
-        post.run(nodaka[i]['schoolcode'], UA, cook)
-    except requests.exceptions.ConnectionError:
-        print("---为 " + nodaka[i].get("stucode") + " 打卡失败")
-print("\n所有人打卡完成")
+# 下面是自动打卡模块
+# for i in range(0, len(nodaka)):
+#     print("开始为 " + nodaka[i].get("stucode") + " 打卡")
+#     try:
+#         cook = sign.login(nodaka[i], UA)
+#         post.run(nodaka[i]['schoolcode'], UA, cook)
+#     except requests.exceptions.ConnectionError:
+#         print("---为 " + nodaka[i].get("stucode") + " 打卡失败")
+# print("\n所有人打卡完成")
